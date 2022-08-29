57e95012f11995cf86d74b88c58a305c8bb2b5bc
manasa <manasa@axisrooms.com>
2022-08-29 11:03:14 +0530

    Cors config added

--- src/main/java/com/axisrooms/controller/itemController.java
+++ src/main/java/com/axisrooms/controller/itemController.java
@@ -1,6 +1,8 @@
 package com.axisrooms.controller;
 
+import com.axisrooms.model.ItemPriceModel;
 import com.axisrooms.model.Response;
+import com.axisrooms.model.SubscriptionModel;
 import com.chargebee.models.ItemFamily;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,12 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/v1/chargebee/items")
 @Api(description = "Api to communicate with chargebee Microservice from Channel Manager")
 @Slf4j
@@ -65,5 +67,23 @@ public class itemController {
             responseEntity = new ResponseEntity<>(new Response(), HttpStatus.SERVICE_UNAVAILABLE);
         }
         return response;
+    }
+
+    @PostMapping(
+            path = "/createItemPrice",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> createItemPrice(@Valid @RequestBody ItemPriceModel itemPriceRequest) {
+        log.info("inside createItemPrices Request Received is ====>"+itemPriceRequest);
+        ResponseEntity<?> responseEntity;
+        try {
+            Response response = itemManager.createItemPrice(itemPriceRequest);
+            responseEntity = new ResponseEntity<>(itemPriceRequest, HttpStatus.OK);
+        } catch (Throwable throwable) {
+            log.error("Encountered exception while create subscription", throwable);
+            responseEntity = new ResponseEntity<>(itemPriceRequest, HttpStatus.SERVICE_UNAVAILABLE);
+        }
+        return responseEntity;
     }
 }
